f7f8d07ac1ebc92a8043098c9a7725e2e56075b2
Guillaume Gendre <dzb.rtz@gmail.com>
2015-05-09 17:45:23 +0200

    importing Hello World iOS

---
+++ HelloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by Sébastien Vitard on 20/04/2015.
+//  Copyright (c) 2015 Cobaltians. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
