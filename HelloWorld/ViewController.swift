f8cb736a5961957389cb1c6406a8d6d4d638fdf4
AntoineBarroux <antoine.barroux@gmail.com>
2016-10-12 14:41:04 +0200

    Swift Helloworld Sample

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by antoine on 12/10/2016.
+//  Copyright © 2016 Cobaltians. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: CobaltViewController {
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        //On set le resourcePath de cobalt
+        Cobalt.setResourcePath((Bundle.main.resourcePath! + "/common/"))
+        initWithPage("index.html", andController: "default")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
